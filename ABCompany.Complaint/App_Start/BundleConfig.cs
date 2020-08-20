7968f9790829cae663006b423786cb0c0938ab43
Radwan Ayoub <radwan-ayoub@outlook.com>
2020-08-20 21:02:38 +0300

    updated the layout and the project structure also added some required file

--- ABCompany.Complaint/App_Start/BundleConfig.cs
+++ ABCompany.Complaint/App_Start/BundleConfig.cs
@@ -8,23 +8,39 @@ namespace ABCompany.Complaint
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            //All JS
+            //All Plugins
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/js/jquery.min.js",
+                        "~/Scripts/js/popper.min.js",
+                        "~/Scripts/js/jquery.magnific-popup.min.js",
+                        "~/Scripts/js/smoothscroll.js",
+                        "~/Scripts/js/form-validator.min.js",
+                        "~/Scripts/js/contact-form-script.js",
+                        "~/Scripts/js/isotope.min.js",
+                        "~/Scripts/js/images-loded.min.js",
+                        "~/Scripts/js/custom.js",
+                        "~/Scripts/js/slider-index.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
+            //Bootstrap JS
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/js/bootstrap.min.js"));
 
+            //Site CSS
+            //Bootstrap CSS
+            //Responsive CSS
+            //Custom CSS
+            //Pogo Slider CSS
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                "~/Content/css/bootstrap.min.css",
+                "~/Content/css/pogo-slider.min.css",
+                "~/Content/css/style.css",
+                "~/Content/css/responsive.css",
+                "~/Content/css/custom.css"));
         }
     }
 }
